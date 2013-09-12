2b67250af742686e3bf8e036db5e111811764126
CpuKnows <jamaxwell1990@gmail.com>
2013-09-12 00:20:49 -0500

    closes #2 Class based views
    Refactored code to generic class based views
    closes #2

--- game_data/urls.py
+++ game_data/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
 from game_data import views
+from game_data.views import *
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^submit_game/$', views.submit_game, name='submit_game'),
-                       url(r'^successful_submit/$', views.successful_submit, name='successful_submit'),
-					   url(r'^hero/(?P<hero_name_in>[a-zA-Z]+)/$', views.hero_detail, name='hero_detail'),
-                       url(r'^player/(?P<player_name_in>[a-zA-Z]+)/$', views.player_detail, name='player_detail'),)
+                       url(r'^$', IndexView.as_view(), name='index'),
+                       url(r'^submit_game/$', GameCreate.as_view(), name='submit_game'),
+                       #url(r'^successful_submit/$', views.successful_submit, name='successful_submit'),
+                       url(r'^successful_submit/$', TemplateView.as_view(template_name = 'game_data/successful_submit.html'), name='successful_submit'),
+                       url(r'^hero/(?P<hero_name>[\w-]+)/$', HeroDetail.as_view(), name='hero_detail'),
+                       url(r'^player/(?P<player_name>[\w-]+)/$', PlayerDetail.as_view(), name='player_detail'),)
--- game_data/views.py
+++ game_data/views.py
@@ -2,49 +2,53 @@ from django import forms
 from django.forms import ModelForm
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.views import generic
+from django.views.generic.edit import CreateView
 
 from game_data.models import *
 
-def index(request):
-	hero_list = Hero.objects.order_by('hero_name')
-	context = {'hero_list':hero_list}
-	return render(request, 'game_data/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'game_data/index.html'
+	context_object_name = 'latest_game_list'
 	
-def hero_detail(request, hero_name_in):
-	hero = get_object_or_404(Hero, hero_name = hero_name_in)
-	#hero_player_list = get_list_or_404(Hero, hero_name = hero_name_in)
-	hero_game_list = Games.objects.filter(hero = hero).order_by('last_hits', 'gpm','pub_date')
-	context = {'hero': hero, 'hero_game_list': hero_game_list}
-	return render(request, 'game_data/hero_detail.html', context)
+	def get_queryset(self):
+		return Games.objects.order_by('-pub_date')[:5]
 	
-def player_detail(request, player_name_in):
-	player = get_object_or_404(Player, player_name = player_name_in)
-	#player_hero_list = Hero.objects.filter(player = player)
-	player_game_list = Games.objects.filter(player = player).order_by('-pub_date')
-	context = {'player': player, 'player_game_list': player_game_list}
-	return render(request, 'game_data/player_detail.html', context)
-	
-def get_hero_choices():
-	return Hero.objects.all().order_by('hero_name')
-	
-def get_player_choices():
-	return Player.objects.all().order_by('player_name')
+	def get_context_data(self, **kwargs):
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context['hero_list'] = Hero.objects.all()
+		return context
 		
-def submit_game(request):
-	if request.method == 'POST':
-		form = GameForm(request.POST)
+	
+class HeroDetail(generic.DetailView):
+	model = Hero
+	template_name = 'game_data/hero_detail.html'
+	slug_field = 'hero_name'
+	slug_url_kwarg = 'hero_name'
+	
+	def get_context_data(self, **kwargs):
+		context = super(HeroDetail, self).get_context_data(**kwargs)
+		hero = get_object_or_404(Hero, hero_name = self.kwargs['hero_name'])
+		context['hero_game_list'] = Games.objects.filter(hero = hero).order_by('-last_hits', '-gpm','-pub_date')
+		return context
 		
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect(reverse('game_data:successful_submit'))
-			
-	else:
-		form = GameForm()
 	
-	context = {'form': form}
-	return render(request, 'game_data/submit_game.html', context)
+class PlayerDetail(generic.DetailView):
+	model = Player
+	template_name = 'game_data/player_detail.html'
+	slug_field = 'player_name'
+	slug_url_kwarg = 'player_name'
 	
-def successful_submit(request):
-	return render(request, 'game_data/successful_submit.html', {})
+	def get_context_data(self, **kwargs):
+		context = super(PlayerDetail, self).get_context_data(**kwargs)
+		player = get_object_or_404(Player, player_name = self.kwargs['player_name'])
+		context['player_game_list'] = Games.objects.filter(player = player).order_by('-last_hits', '-gpm','-pub_date')
+		return context
+		
+
+class GameCreate(CreateView):
+	model = Games
+	template_name = 'game_data/submit_game.html'
+	fields = ['player', 'hero', 'last_hits', 'gpm', 'pub_date']
+	
